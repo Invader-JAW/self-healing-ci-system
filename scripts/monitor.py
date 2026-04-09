47f9f17e0b1d59162a60494a9684aaffeab11ff9
Josh <josh.fabrizio@trpz.com>
2026-04-09 08:53:18 -0700

    Added run once to break out of the loop

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -1,6 +1,9 @@
 import docker
 import time
+import sys
 client = docker.from_env()
+
+RUN_ONCE = "--once" in sys.argv
 
 while True:
     containers = client.containers.list(all=True)
@@ -11,4 +14,6 @@ while True:
             c.restart()
             with open("log.txt", "a") as f:
                 f.write(f"{c.name} restarted\n")
+    if RUN_ONCE:
+        break
     time.sleep(5)
